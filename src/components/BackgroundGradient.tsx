28b7389213f3172280d3520b503f452fcce513b0
Rob Harding <robbie.harding@hotmail.com>
2023-11-16 14:43:41 -0500

    Final gradient change for now!

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -27,7 +27,7 @@ const BackdropColors: FC<BackdropColorsProps> = ({}) => {
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
-            className="relative aspect-[1155/678] -translate-x-1/2 rotate-[210deg] bg-gradient-to-tr from-[#f97316] to-[#9089fc] opacity-40 left-[calc(50%)-1/2] w-[36.125rem] sm:w-[72.1875rem] "
+            className="relative aspect-[1155/678] -translate-x-1/2 rotate-[210deg] bg-gradient-to-tr from-[#f97316] to-[#9089fc] opacity-40 w-[36.125rem] sm:w-[72.1875rem] "
           />
         </div>
       </div>
