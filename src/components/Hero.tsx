ee870db41432e0317113139d75312b7a0a0c9599
Rob Harding <robbie.harding@hotmail.com>
2023-11-16 13:34:55 -0500

    Happy with hero section for now

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,38 +1,65 @@
+"use client";
+
 import Image from "next/image";
 import { FC } from "react";
-import { Button } from "./ui/button";
+import { motion } from "framer-motion";
 import {
   ArrowRight,
-  ArrowRightFromLine,
   Download,
   Github,
   Linkedin,
+  MoveRight,
 } from "lucide-react";
+
+import { Button, buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = ({}) => {
   return (
     <section className="w-full sm:w-[36rem] md:w-fit mx-auto">
-      <div className="flex w-full items-center justify-center gap-8">
+      <div className="flex w-full items-center justify-center gap-8 select-none">
         <div className="relative">
-          <Image
-            src="/images/profile.jpg"
-            alt="Profile"
-            width="192"
-            height="192"
-            quality={100}
-            priority={true}
-            className="h-32 w-32 sm:h-48 sm:w-48 md:h-52 md:w-52 object-cover rounded-full border-[0.35rem] border-white shadow-xl"
-          />
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2, type: "tween" }}
+          >
+            <Image
+              src="/images/profile.jpg"
+              alt="Profile"
+              width="192"
+              height="192"
+              quality={100}
+              priority={true}
+              className="h-32 w-32 sm:h-48 sm:w-48 md:h-52 md:w-52 object-cover rounded-full border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
 
-          <span className="absolute right-0 bottom-[0.35rem] text-3xl sm:text-5xl">
+          <motion.span
+            className="absolute right-0 bottom-[0.35rem] text-3xl sm:text-5xl"
+            initial={{ rotate: -30, opacity: 0, scale: 0 }}
+            animate={{ rotate: 0, opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
             👋
-          </span>
+          </motion.span>
         </div>
 
-        <div>
-          <h1 className="text-5xl sm:text-7xl md:text-8xl font-bold">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
+          className="select-text"
+        >
+          <h1 className="text-5xl sm:text-7xl md:text-8xl font-bold !leading-[2.5rem] sm:!leading-[4rem] md:!leading-[5rem]">
             ROB
             <br />
             HARDING
@@ -41,34 +68,53 @@ const Hero: FC<HeroProps> = ({}) => {
             <span className="hidden sm:inline-block">Fullstack</span> Web
             Developer
           </h2>
-        </div>
+        </motion.div>
       </div>
-      <div className="flex justify-center gap-2 sm:gap-3 mt-8 w-[10rem] sm:w-[28rem] mx-auto">
-        <Button
-          className="flex flex-1 gap-2  items-center text-xs sm:text-base"
-          size="sm"
+      <motion.div
+        className="select-none flex justify-center gap-2 sm:gap-3 mt-6 sm:mt-10 md:mt-12 w-[10rem] sm:w-[28rem] md:w-[36rem] mx-auto"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "flex flex-1 gap-2 items-center text-xs sm:text-base group"
+          )}
+          href="/"
         >
-          Contact me here <ArrowRight className="w-4 h-4" />
-        </Button>
-        <Button
-          className="flex gap-2 flex-1 items-center text-xs sm:text-base bg-white text-foreground hover:bg-gray-100 border-white border-opacity-40"
-          size="sm"
+          Contact me here{" "}
+          <MoveRight className="w-4 h-4 mt-[0.125rem] group-hover:translate-x-1 duration-200" />
+        </Link>
+        <Link
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "flex gap-2 flex-1 items-center text-xs sm:text-base bg-white text-foreground hover:bg-gray-100 border-white border-opacity-40 group"
+          )}
+          href="/"
         >
-          Download CV <Download className="w-4 h-4" />
-        </Button>
-        <Button
-          size="sm"
-          className="bg-white text-foreground hover:bg-gray-100 border-white border-opacity-40"
+          Download CV{" "}
+          <Download className="w-4 h-4 group-hover:scale-110 duration-200" />
+        </Link>
+        <Link
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "bg-white text-foreground hover:bg-gray-100 border-white border-opacity-40"
+          )}
+          href="/"
         >
-          <Linkedin className="w-4 h-4" />
-        </Button>
-        <Button
-          size="sm"
-          className="bg-white text-foreground hover:bg-gray-100 border-white border-opacity-40"
+          <Linkedin className="w-4 h-4 " />
+        </Link>
+        <Link
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "bg-white text-foreground hover:bg-gray-100 border-white border-opacity-40"
+          )}
+          href="/"
         >
           <Github className="w-4 h-4" />
-        </Button>
-      </div>
+        </Link>
+      </motion.div>
     </section>
   );
 };
