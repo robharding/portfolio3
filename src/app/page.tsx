52291001790e0c14ba3defc6a8ce13af782aa57d
Rob Harding <robbie.harding@hotmail.com>
2023-11-16 14:46:46 -0500

    Projects placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <SectionDivider />
       <About />
+      <Projects />
     </main>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import SectionHeading from "./SectionHeading";
+
+interface ProjectsProps {}
+
+const Projects: FC<ProjectsProps> = ({}) => {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div></div>
+    </section>
+  );
+};
+
+export default Projects;
